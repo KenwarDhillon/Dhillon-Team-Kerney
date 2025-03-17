1fb970581a844691e13cf3cbfa5539ded9a5945f
dhillon_1015240 <kennydhillonpatino@gmail.com>
2025-03-17 07:25:18 -0700

    Finished particle.h for now-KD

--- particle.h
+++ particle.h
@@ -22,22 +22,47 @@ class Particle {
 			dx = 0.0;
 			dy = 0.0;
 			lifetime = 0;
-			// FIXME: Add and finish the particle type;-KD
-			type = ParticleType::STREAMER;
+			type = ParticleType::BALLISTIC;
 		}
 
 		Particle(double new_x, double new_y, double new_dx, double new_dy, int new_lifetime, ParticleType new_type) : x(new_x), y(new_y), dx(new_dx), dy(new_dy), lifetime(new_lifetime), type(new_type) {} // Paramterized constructor;-KD
 
 		// My getters are below;-KD
 
-		double get_x() const { return x };
-		double get_y() const { return y };
-		double get_dx() const { return dx };
-		double get_dy() const { return dy };
-		int get_lifetime() const { return lifetime };
-		ParticleType get_type() const { return type };
+		double get_x() const { return x; }
+		double get_y() const { return y; }
+		double get_dx() const { return dx; }
+		double get_dy() const { return dy; }
+		int get_lifetime() const { return lifetime; }
+		ParticleType get_type() const { return type; }
 
 		// My setters are below;-KD
+
+		void set_x(double new_x) {
+			x = new_x;	
+		}
+		
+		void set_y(double new_y) {
+			y = new_y;	
+		}
+		
+		void set_dx(double new_dx) {
+			dx = new_dx;	
+		}
+		
+		void set_dy(double new_dy) {
+			dy = new_dy;	
+		}
+		
+		void set_lifetime(int new_lifetime) {
+			lifetime = new_lifetime;	
+		}
+		
+		void set_type(ParticleType new_type) {
+			type = new_type;	
+		}
+
+		// My stub methods are below;-KD
 
 		void drawParticle() const; // This draws the particles on the screen;-KD
 		void physics() const;
