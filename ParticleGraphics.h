ed67a21298688407b6f24039eacdccbfdff25474
ridah-chaudhry <0972952@my.scccd.edu>
2025-03-26 13:44:41 -0700

    Update ParticleGraphics.h

--- ParticleGraphics.h
+++ ParticleGraphics.h
@@ -13,7 +13,7 @@ public:
 	void setColor(const string& color) {currentColor = color;}  
 	string getColor () const {return currentColor;}								//getter & setter
 
-	void drawPoint(double x, double y, int r, int b, int g) const;
+	void drawParticle(double x, double y, int r, int b, int g) const;
 	void drawLine(double x1, double y1, double x2, double y2, int r, int b, int g) const;
 	void drawRectangle(double x1, double y1, double x2, double y2, int r, int b, int g) const;
 //	void drawOval(double x1, double y1, double x2, double y2) const;
